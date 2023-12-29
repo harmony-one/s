343b7e95a9e557e11d7ae1ee82dcc5add6360bca
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2023-12-29 00:49:50 -0800

    fix: subscription logic

--- s-bridge/relayer/index.ts
+++ s-bridge/relayer/index.ts
@@ -1,40 +1,52 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express from 'express';
-import Web3 from 'web3';
-import { EventData } from 'web3-eth-contract';
+import { ethers } from 'ethers';
 
 const app = express();
 const port = 3000;
 
-const arbitrumWSS = process.env.ARBITRUM_SEPOLIA_WSS as string;
-
-const arbitrumProvider = new Web3.providers.WebsocketProvider(arbitrumWSS);
-
-const web3Arbitrum = new Web3(arbitrumProvider);
+const arbitrumRPC = process.env.ARBITRUM_SEPOLIA_RPC as string;
+const provider = new ethers.providers.JsonRpcProvider(arbitrumRPC);
 
 const arbitrumLogs: string[] = [];
+const contractAddress = process.env.ARBITRUM_SEPOLIA_ADDRESS as string;
+const contractArtifact = require("../bridge/artifacts/contracts/Bridge.sol/Bridge.json");
+const contractInterface = new ethers.utils.Interface(contractArtifact.abi);
 
-const arbitrumAddress = process.env.ARBITRUM_SEPOLIA_ADDRESS as string;
+let lastCheckedBlock: number | null = null;
 
-const arbitrumJson = require("../bridge/artifacts/contracts/Bridge.sol/Bridge.json");
-const arbitrumContract = new web3Arbitrum.eth.Contract(arbitrumJson.abi, arbitrumAddress);
+async function pollTransactions() {
+	try {
+			const latest = await provider.getBlockNumber();
+			if (lastCheckedBlock === null || lastCheckedBlock !== latest) {
+					const block = await provider.getBlockWithTransactions(latest);
+					const contractTransactions = block.transactions.filter(tx => tx.to && tx.to.toLowerCase() === contractAddress.toLowerCase());
 
-arbitrumContract.events.allEvents({
-    fromBlock: 'latest'
-})
-.on('data', (event: EventData) => {
-    arbitrumLogs.push(JSON.stringify(event));
-    console.log('New Arbitrum Event:', event);
-})
-.on('error', (error: Error) => {
-    console.error('Arbitrum Event Error:', error);
-});
+					for (const tx of contractTransactions) {
+							const receipt = await provider.getTransactionReceipt(tx.hash);
+							for (const log of receipt.logs) {
+									if (log.address.toLowerCase() === contractAddress.toLowerCase()) {
+											const parsedLog = contractInterface.parseLog(log);
+											arbitrumLogs.push(JSON.stringify(parsedLog));
+											console.log('Event emitted in tx:', parsedLog);
+									}
+							}
+					}
+
+					lastCheckedBlock = latest;
+			}
+	} catch (error) {
+			console.error('Error:', error);
+	}
+}
+
+setInterval(pollTransactions, 1000); // Poll every second
 
 app.get('/', (req, res) => {
     res.send(`
         <h1>Relayer Service</h1>
-        <p><strong>Arbitrum Sepolia RPC WSS:</strong> ${arbitrumWSS}</p>
-        <p><strong>Arbitrum Sepolia Contract Address:</strong> ${arbitrumAddress}</p>
+        <p><strong>Arbitrum Sepolia RPC URL:</strong> ${arbitrumRPC}</p>
+        <p><strong>Arbitrum Sepolia Contract Address:</strong> ${contractAddress}</p>
     `);
 });
 
