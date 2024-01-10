b2c45e1fa7d6ad6aa6fe0f7f92b69622d0ca114d
artemkolodko <temakolodko@gmail.com>
2024-01-10 14:54:13 +0000

    Add price provider (CoinGecko)

---
+++ moc/src/services/PriceProvider.ts
@@ -0,0 +1,43 @@
+import axios from 'axios'
+
+interface CoinGeckoPriceResponse {
+  [key: string]: {
+    [key: string]: number
+  }
+}
+
+export class PriceProvider {
+  private readonly currency = 'usd'
+  private readonly tokensList: string[]
+  priceData: CoinGeckoPriceResponse = {}
+
+  constructor(tokensList = ['harmony']) {
+    this.tokensList = tokensList
+    this.pollPrice()
+  }
+
+  async getTokenPrice(token: string) {
+    if(this.priceData[token] && this.priceData[this.currency]) {
+      return this.priceData[this.currency]
+    }
+    console.warn(`Unavailable price for token ${token}. Using default value = 0.`)
+    return 0
+  }
+
+  private async pollPrice() {
+    try {
+      this.priceData = await this.fetchTokensPrice(this.tokensList)
+    } catch (e) {
+      console.error('Price polling error:', e)
+    } finally {
+      setTimeout(() => this.pollPrice(), 10 * 60_000)
+    }
+  }
+
+  private async fetchTokensPrice(tokens: string[], currency = this.currency) {
+    const ids = tokens.join(',')
+    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${ids}&vs_currencies=${currency}`
+    const { data } = await axios.get<CoinGeckoPriceResponse>(url)
+    return data
+  }
+}
