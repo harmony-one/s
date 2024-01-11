fc8715725482deabb8570ff373b8999ef0525ce1
Sun Hyuk Ahn <joycaleb9705@berkeley.edu>
2024-01-11 10:53:14 -0800

    config: moc to flip

--- moc/src/app.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-
-const app = express();
-
-// app.use(middleware);
-// app.use('/api/someRoute', someRouteHandler);
-
-export default app;
--- moc/src/types/customTypes.ts
+++
@@ -1,3 +0,0 @@
-import { ethers } from "ethers";
-
-export type TransactionResponse = ethers.providers.TransactionResponse;
