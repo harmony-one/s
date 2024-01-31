8834c3ec559f21053f962b2fd7377a4a5439f1cb
Rika <15hatoyar@gmail.com>
2024-01-31 10:32:56 -0800

    separate front back

---
+++ s-game/client/src/Screens/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { TextField } from '@mui/material';
+
+const Home = () => {
+  return (
+    <div>
+        <TextField id="outlined-basic" label="Their API" variant="outlined" />
+    </div>
+  )
+}
+
+export default Home
