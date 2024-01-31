ab9d21feacfd95559d4b05b0765b2d869f28a260
Aishlia <nai.julia@gmail.com>
2024-01-31 10:41:06 -0800

    Add 0 and backspace

--- s-PmF/pmf/src/App.js
+++ s-PmF/pmf/src/App.js
@@ -11,17 +11,37 @@ function App() {
     }
   };
 
-  const numberPad = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
+  const handleBackspace = () => {
+    setInputValue(inputValue.slice(0, -1));
+  };
+
+  const numberButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((number) => (
     <button key={number} onClick={() => handleNumberClick(number)}>
       {number}
     </button>
   ));
 
+  const zeroButton = (
+    <button key="0" className="zero-button" onClick={() => handleNumberClick(0)}>
+      0
+    </button>
+  );
+
+  const backspaceButton = (
+    <button key="backspace" className="backspace-button" onClick={handleBackspace}>
+      ⌫
+    </button>
+  );
+
   return (
     <div className="App">
       <div className="display">{displayValue}</div>
       <input type="text" value={inputValue} readOnly />
-      <div className="number-pad">{numberPad}</div>
+      <div className="number-pad">
+        {numberButtons}
+        {zeroButton}
+        {backspaceButton}
+      </div>
     </div>
   );
 }
